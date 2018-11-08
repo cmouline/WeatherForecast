4d0cf57ebf70e717171ea927f993b7fd145da555
Moulinet Chloë <cmoulinet@gmail.com>
2018-11-08 12:26:50 +0100

    Add kingfisher

--- WeatherForecast/WeatherForecast/UI/CityWeatherViewController.swift
+++ WeatherForecast/WeatherForecast/UI/CityWeatherViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import SwiftyJSON
+import Kingfisher
 
 class CityWeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableview: UITableView!
     
+    let iconUrl = "https://openweathermap.org/img/w/%@.png"
     var weatherData: JSON?
-    var iconData: [UIImage]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,19 +42,26 @@ class CityWeatherViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherTableViewCell
         
-        if let weatherData = weatherData?["list"][indexPath.row], let iconData = iconData {
-            cell.weatherIconImageView.image = iconData[indexPath.row]
-
-            if let timestamp = weatherData["dt"].double {
-                cell.mainInformationsLabel.text = DateHelper.formatTimestamp(timestamp, format: .fullDateTime)
+        if let weatherData = weatherData?["list"][indexPath.row] {
+            
+            // Get weather icon and cache it with Kingfisher
+            if let iconRef = weatherData["weather"][0]["icon"].string,
+               let url = URL(string: String(format: iconUrl, iconRef)) {
+                cell.weatherIconImageView.kf.setImage(with: url)
             }
             
+            // Set date and time label
+            if let timestamp = weatherData["dt"].double {
+                cell.dateLabel.text = DateHelper.formatTimestamp(timestamp, format: .fullDateTime)
+            }
+            
+            // Set textual informations and temperature
             let weatherInfo = weatherData["weather"][0]
             
             if let temperature = weatherData["main"]["temp"].double?.description,
-                let mainInfo = weatherInfo["main"].string,
-                let descriptionInfo = weatherInfo["description"].string {
-                cell.textDescriptionLabel.text = "\(mainInfo): \(descriptionInfo) 🌡 \(temperature) º\(RequestManager.shared.degreeUnit.string)"
+               let mainInfo = weatherInfo["main"].string,
+               let descriptionInfo = weatherInfo["description"].string {
+                cell.informationsLabel.text = "\(mainInfo): \(descriptionInfo) 🌡 \(temperature) º\(RequestManager.shared.degreeUnit.description)"
             }
         }
 
